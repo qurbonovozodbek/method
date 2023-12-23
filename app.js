ff57b13b4ae6c82e5204d97eb187d70f9e0ab406
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2023-12-23 20:26:48 +0500

    method

---
+++ app.js
@@ -0,0 +1,58 @@
+// 1 - masala
+
+// let arr =['salom', 'alik', 'qale']
+
+// function somarr(arg) {
+//   let a = arg.indexOf('qale')
+// if (a > 1) {
+//   a = a**2;
+// } else {
+//   a = 'mavjud emas'
+// }
+// return a;
+// }
+// console.log(somarr(arr));
+
+// 2 - masala
+
+// let a = [1, 2, 3]
+// let b = [4, 5, 6]
+// let c = [7, 8]
+
+// function SomArr(arg, arg2, arg4) {
+//     let max = b.concat(c, a)
+//     return max;
+// }
+// console.log(SomArr(a,b,c,));
+
+// 3 - masala
+
+// let  arr = [1, 4, 7, 8, 9]
+// let a = arr.find((value) => {
+//     let som = value > 8;
+//     return som;
+// })
+// console.log(a ** 2);
+
+// 4 - masala
+
+// let max = 0;
+// let arr = [1, 2, 4, 5, 6, 7, 8, 9, 11, 15]
+// let a = arr.forEach((value) => {
+//     if (value % 2 == 1) {
+//         max++;
+//     }
+// })
+// console.log(max);
+
+// 5 - masala
+
+// let arr = ['dars', 'for', 'max', 'domla']
+// let lastlatter = []
+// let a = arr.forEach((value) => {
+//     let som = value.slice(-1);
+//     lastlatter.push(som)
+// })
+// console.log(lastlatter);
+
+// 6 - masala
